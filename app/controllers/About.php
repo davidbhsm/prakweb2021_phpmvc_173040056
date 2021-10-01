3d992e8cdb2fc763939e08d2835b756031ac14cd
davidbhsm <davidbhism@gmail.com>
2021-10-01 18:04:13 +0700

    Menyelsaikan video 5 pada pertemuan 4

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,7 +1,20 @@
 <?php
 
-class About {
+class About extends Controller {
+    public function index($nama = 'david', $status = 'mahasiswa', $umur = 22) {
+        $data['nama']=$nama;
+        $data['status']=$status;
+        $data['umur']=$umur;
+        $data['judul'] = 'About Me';
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer');
+    }
+
     public function page() {
-        echo 'About/page';
+        $data['judul'] = 'Pages';
+        $this->view('templates/header', $data);
+        $this->view('about/page');
+        $this->view('templates/footer');
     }
 }
